fcaa41bdccd3e3ed7f3d227abcc405e88a56de34
Saurabh Patil <85858942+saurabh00031@users.noreply.github.com>
2022-04-10 21:31:39 +0530

    abstract classes changes

--- Java_Core/tut53AbstractClasses.java
+++ Java_Core/tut53AbstractClasses.java
@@ -12,6 +12,19 @@
 //one abstract method makes whole class abstract andf its is  mentioned in name of class
 //as abstract
 
+//in java,
+      //abstract method is method that is declared without an implementation //
+
+
+
+//Abstract class ....If am class includes abstract methods,
+    //class itself must be declared abstract
+    
+    
+    
+//one abstract method makes whole class abstract andf its is  mentioned in name of class
+//as abstract
+
 abstract class Base2
 {
     Base2()
@@ -50,11 +63,20 @@ abstract class Child3 extends Child2
 
 public class Main
 {
-public static void main (String[] args)
-{
+  public static void main (String[] args)
+  {
+   Child2 ch2=new Child2();
+   ch2.greet();
+   
+   /*
+     Child3 is abstract so we can't call from Main class...........
+     
+     
+     Child3 ch3=new Child3();
+   ch3.greet();
+   
+   */
    
    
-   
+   }
 }
-}
-
